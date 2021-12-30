9d9c75a8e490a5b8f20bf1f877814ecbac9e5e9f
Jake Wnuk <jakewnuk@gmail.com>
2021-12-29 20:05:26 -0500

    Initial commit
    release of version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,143 @@
-# PwdStat
- CLI tool for identifying systemic password usage, creating password masks, and analyzing cracked password samples with human readable statistics to help defenders.
+<h1 align="center">
+PwdStat
+ </h1>
+
+CLI tool for identifying systemic password usage, creating password masks, and analyzing cracked password samples with human readable statistics to help defenders.
+
+## Getting Started
+
+-   [Usage](#usage)
+-   [Install](#install)
+-   [Output](#output)
+
+## Usage
+```sh
+pwdstat.py -h
+
+usage: pwdstat.py [-h] [-i INPUT] [-c COMPARE] [-o OUTPUT] [-f] [-q]
+
+Tool for identifying systemic password usage, creating password masks, and analyzing cracked password samples with
+human readable statistics
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input list of passwords.
+  -c COMPARE, --compare COMPARE
+                        Directory of lists to compare against.
+  -o OUTPUT, --output OUTPUT
+                        Prints CSV files to directory. The default is cwd.
+  -f, --filter          Filter subpar from results and bottom 0.1 percent of masks and tokens.
+  -q, --quiet           Hides banner
+```
+Take a list of cracked passwords and analyze them.
+```
+pwdstat.py -i cracked_wordlist.txt
+
+cat cracked_wordlist | pwdstat.py
+```
+Filter out passwords that do not meet minimum complexity requirements and the bottom 0.1% of tokens and masks.
+```
+cat cracked_wordlist | pwdstat.py -f
+```
+Compare input list against a directory of wordlists and mark ones that appear in them.
+```
+pwdstat.py -i cracked_wordlist.txt -c ./breach-data/
+```
+Input and compare but also output CSV files with data to a directory.
+```
+pwdstat.py -i cracked_wordlist.txt -c ./breach-data -o pwd_output
+ __        __   __  ___      ___
+|__) |  | |  \ /__`  |   /\   |
+|    |/\| |__/ .__/  |  /~~\  |
+[*] Password Stats:
+[*] Reminder the sample is ONLY cracked passwords and data points should be reflected on as so
+[*] Microsoft minimum password complexity requires 3 of the following criteria: 1 lowercase, 1 uppercase, 1 digit, and 1 special character.
+There are 6443 passwords in the sample and the average complexity is 2.0/4 and the average length is 8.4
+5181 passwords were considered subpar and did not meet minimum password requirements and had an average length of 7.9
+46 passwords met the minimum complexity requirements and had an average length of 6.8
+983 passwords met or exceeded minimum complexity requirements and had an average length of 9.2
+143 passwords met or exceeded minimum complexity requirements and had a strong password length averaging 12.8
+43 passwords met or exceeded minimum complexity requirements and had a very strong password length averaging 19.9
+47 passwords well exceeded minimum complexity requirements and had a fortified password length averaging 23.0
+[*] Password Lookup:
+4384 passwords were also in HIBP-Top-7M.txt
+2783 passwords were also in rockyou.txt
+[*] Reused Passwords:
+Fall2021 occurred 130 times
+Password1 occurred 27 times
+password occurred 13 times
+Password$ occurred 4 times
+Winter2021! occurred 4 times
+Fall2021! occurred 4 times
+zaq1!@#$ occurred 4 times
+rj143 occurred 3 times
+[*] Common Tokens and Words in Passwords:
+The token fall2021 was used 136 times
+The token $ was used 79 times
+The token password1 was used 30 times
+The token zaq1 was used 26 times
+The token password was used 19 times
+The token * was used 6 times
+The token dcshoecousa was used 6 times
+The token winter2021 was used 6 times
+[*] Common Password Masks:
+342 passwords used the mask ?d?d?d?d?d?d
+For example: 640512, 118217, and 770884
+325 passwords used the mask ?l?l?l?l?l?l?d?d
+For example: jelena15, memory66, and jelena15
+299 passwords used the mask ?l?l?l?l?l?l
+For example: rizqie, rizqie, and dcshew
+259 passwords used the mask ?l?l?l?l?l?l?l?l
+For example: dcsgjskg, dcrowdie, and dcrooter
+251 passwords used the mask ?l?l?l?l?l?d?d?d?d?d
+For example: frame12175, glass80034, and shark57315
+233 passwords used the mask ?l?l?l?l?l?l?l
+For example: chogori, chogori, and dcshete
+184 passwords used the mask ?l?l?l?l?l?l?l?d?d
+For example: dcshoes21, dcshoes13, and dcshoes01
+165 passwords used the mask ?l?l?l?l?l?d?d
+For example: jessi03, jessi03, and dcsan13
+```
+
+
+## Install
+**PwdStat** works on Windows and *Nix systems and requires Python.
+```
+git clone 
+```
+```
+pip install -r requirements.txt
+```
+## Output
+The `-o` flag is used to direct the CSV output files to a directory by default **PwdStat** does not print output files. Five output files will be created with the following keys:
+
+### passwords.csv
+All of the passwords from the set tagged
+|Password|Class|Complexity|Length|Mask|Is In *|
+|---|---|--|--|--|--|
+|Password|Rating given to password based on criteria|Complexity rating out of four|Length of password|Password mask in Hashcat format|Is the password in this file (repeated for all files in directory)|
+
+### passwords_agg.csv
+All of the passwords from the set aggregated by password to remove duplicates
+|Password|Count|Complexity|Length|
+|---|---|--|--|
+|Password|Occurrences in list|Average complexity rating|Average length of password
+
+### common_tokens.csv
+Words and tokens from the passwords sorted by count. Passwords are passed to NLTK for parsing.
+|Tokens|Count|
+|---|---|
+|Password token|Count of token in all passwords|
+
+### password_classes.csv
+All of the passwords from the set aggregated by class
+|Class|Count|Complexity|Length|
+|---|---|--|--|
+|Password class|Occurrences in list|Average complexity rating|Average length of password
+
+### password_masks.csv
+All of the passwords from the set aggregated by mask
+|Class|Count|Complexity|Length|
+|---|---|--|--|
+|Password mask|Occurrences in list|Average complexity rating|Average length of password
---
+++ pwdstat.py
@@ -0,0 +1,411 @@
+import argparse
+import os
+import sys
+
+import nltk
+import pandas as pd
+from colorama import init
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+
+def dep_check():
+    """
+    Checks if nltk dependencies are installed
+    """
+    try:
+        word_tokenize('can you parse me')
+    except LookupError:
+        nltk.download('punkt')
+
+
+def message(msg, title=False, stat=False, word=False, banner=False):
+    """
+    Prints formatted text to CLI
+    """
+
+    class Colors:
+        BLUE = '\033[94m'
+        GREEN = "\033[32m"
+        YELLOW = '\033[93m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+
+    banner_text = """ __        __   __  ___      ___ 
+|__) |  | |  \ /__`  |   /\   |  
+|    |/\| |__/ .__/  |  /~~\  |  """
+
+    if title:
+        print(f'{Colors.GREEN}{Colors.BOLD}[*] {msg}{Colors.ENDC}')
+    elif stat:
+        print(f'{Colors.BLUE}{msg}{Colors.ENDC}')
+    elif word:
+        return f'{Colors.YELLOW}{Colors.BOLD}{msg}{Colors.ENDC}{Colors.BLUE}'
+    elif banner:
+        print(f'{Colors.YELLOW}{banner_text}{Colors.ENDC}')
+
+
+class PasswordAnalyzer:
+    """
+    Takes in a list of passwords and analyzes them
+    """
+
+    def __init__(self, password_df):
+        self.compositionType = None
+        self.mask = None
+        self.alphaLst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                         't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.digitsLst = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.specialLst = [' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';',
+                           '<', '=', '>', '?', '@', '[', '\', \']', '^', '_', '`', '{', '|', '}', '~', ']']
+        self.df = password_df
+
+    def classify_passwords(self):
+        """
+        Generates primary columns per password
+        """
+        df['Class'] = [self.test_class(x) for x in df['Password']]
+        df['Complexity'] = [self.test_complexity(x) for x in df['Password']]
+        df['Length'] = df['Password'].apply(len)
+
+    def test_class(self, password_str):
+        """
+        Ranks passwords by length and complexity into a class score
+        :param password_str: password to classify
+        :return: str
+        """
+        digits = 0
+        lower = 0
+        upper = 0
+        special = 0
+        length = len(password_str)
+
+        if any(item in password_str for item in self.digitsLst):
+            for char in self.digitsLst:
+                digits += password_str.count(char)
+        if any(item in password_str for item in self.alphaLst):
+            for char in self.alphaLst:
+                lower += password_str.count(char)
+        if any(item in password_str for item in map(str.upper, self.alphaLst)):
+            for char in map(str.upper, self.alphaLst):
+                upper += password_str.count(char)
+        if any(item in password_str for item in self.specialLst):
+            for char in self.specialLst:
+                special += password_str.count(char)
+
+        if length >= 16:
+            if all(v >= 2 for v in [digits, lower, upper, special]):
+                return "5: strong"
+            elif all(v >= 1 for v in [digits, upper, lower]):
+                return "4: great"
+            elif all(v >= 1 for v in [digits, lower, special]):
+                return "4: great"
+            elif all(v >= 1 for v in [digits, upper, special]):
+                return "4: great"
+            elif all(v >= 1 for v in [lower, upper, special]):
+                return "4: great"
+            else:
+                return "0: subpar"
+        elif length >= 12:
+            if all(v >= 2 for v in [digits, lower, upper, special]):
+                return "4: great"
+            elif all(v >= 1 for v in [digits, upper, lower]):
+                return "3: good"
+            elif all(v >= 1 for v in [digits, lower, special]):
+                return "3: good"
+            elif all(v >= 1 for v in [digits, upper, special]):
+                return "3: good"
+            elif all(v >= 1 for v in [lower, upper, special]):
+                return "3: good"
+            else:
+                return "0: subpar"
+        elif length >= 8:
+            if all(v >= 2 for v in [digits, lower, upper, special]):
+                return "3: good"
+            elif all(v >= 1 for v in [digits, upper, lower]):
+                return "2: okay"
+            elif all(v >= 1 for v in [digits, lower, special]):
+                return "2: okay"
+            elif all(v >= 1 for v in [digits, upper, special]):
+                return "2: okay"
+            elif all(v >= 1 for v in [lower, upper, special]):
+                return "2: okay"
+            else:
+                return "0: subpar"
+        else:
+            if all(v >= 1 for v in [digits, upper, lower]):
+                return "1: minimum"
+            elif all(v >= 1 for v in [digits, lower, special]):
+                return "1: minimum"
+            elif all(v >= 1 for v in [digits, upper, special]):
+                return "1: minimum"
+            elif all(v >= 1 for v in [lower, upper, special]):
+                return "1: minimum"
+            return '0: subpar'
+
+    def test_complexity(self, password_str):
+        """
+        Ranks the complexity score of the password
+        :param password_str: string to rate
+        :return: int
+        """
+        complexity = 0
+        if any(item in password_str for item in self.digitsLst):
+            complexity += 1
+        if any(item in password_str for item in self.alphaLst):
+            complexity += 1
+        if any(item in password_str for item in map(str.upper, self.alphaLst)):
+            complexity += 1
+        if any(item in password_str for item in self.specialLst):
+            complexity += 1
+        return complexity
+
+    def gen_masks(self):
+        """
+        Generates a Hashcat mask for each password as a new column
+        """
+        df['Mask'] = [self.make_mask(x) for x in df['Password']]
+
+    def make_mask(self, password_str):
+        """
+        Generates a Hashcat mask given a string
+        :param password_str: string to make mask of
+        :return: string
+        """
+        mask = ''
+        for i in password_str:
+            if i in self.digitsLst:
+                mask += "?d"
+            elif i in self.alphaLst:
+                mask += "?l"
+            elif i in map(str.upper, self.alphaLst):
+                mask += "?u"
+            elif i in self.specialLst:
+                mask += "?s"
+            else:
+                mask += "?a"
+        return mask
+
+    def tokenize_passwords(self):
+        """
+        Generates a token list for each password as a new column
+        :return: pd.DataFrame
+        """
+        x = [self.gen_tokens(x) for x in df['Password']]
+        y = [str(item[0]) for item in x]
+        df_tokens = pd.DataFrame(y).value_counts(ascending=False).to_frame().set_axis(['Count'], axis=1,
+                                                                                      inplace=False).reset_index()
+        df_tokens.columns = ['Tokens', 'Count']
+        df_tokens.reset_index(drop=True)
+        df_tokens = df_tokens[df_tokens['Count'] > 1]
+        return df_tokens
+
+    @staticmethod
+    def gen_tokens(password_str):
+        """
+        Tokenizes password into a list
+        :param password_str: password to tokenize
+        :return: list
+        """
+        try:
+            words = word_tokenize(password_str)
+            stemmed_words = [stemmer.stem(word) for word in words]
+        except IndexError:
+            stemmed_words = ['']
+        return stemmed_words
+
+    def analyze_passwords(self):
+        """
+        Calls primary analysis functions
+        """
+        self.classify_passwords()
+        self.gen_masks()
+
+    def lookup_directory(self, compare_dir):
+        """
+        Looks up a directory and all files inside for comparison
+        :param compare_dir: directory full of password lists
+        """
+        # try error for file load fail
+        if compare_dir:
+            for i in os.listdir(compare_dir):
+                title = 'Is In ' + i
+                df_compare = pd.read_table(os.path.join(compare_dir, i), header=None, names=['Password'],
+                                           encoding='ISO-8859-1')
+                self.lookup_password(df_compare, title)
+
+    @staticmethod
+    def lookup_password(compare_df, title):
+        """
+        Compares two password lists for shared passwords then reports in a new column
+        :param compare_df: dataframe to compare against
+        :param title: string name of created column
+        :return: none (appends to given df)
+        """
+        df_joined = df.merge(compare_df, how='inner', on=['Password'])
+        lst_joined = list(set(df_joined.Password.unique().tolist()))
+
+        for q in lst_joined:
+            df.loc[(df['Password'] == q), str(title)] = 'TRUE'
+
+    def report(self, filter_lowqual):
+        """
+        Generates aggregate DFs for printing and print stats to CLI
+        :param filter_lowqual: bool to filter subpar results and results less than 0.01% of the sample
+        """
+        if filter_lowqual:
+            df = self.df[self.df['Class'] != '0: subpar']
+        else:
+            df = self.df
+
+        df_tokens = pwdAnalyzer.tokenize_passwords()
+
+        df_class_agg = df.groupby(by='Class').agg(
+            {'Password': 'count', 'Complexity': 'mean', 'Length': 'mean'}).reset_index()
+        df_class_agg.columns = ['Class', 'Password', 'Complexity', 'Length']
+        df_class_agg.reset_index(drop=True)
+        df_class_agg.rename(columns={'Password': 'Count'}, inplace=True)
+
+        df_mask_agg = df.groupby(by='Mask').agg(
+            {'Password': 'count', 'Complexity': 'mean', 'Length': 'mean'}).sort_values(by='Password',
+                                                                                       ascending=False).reset_index()
+        df_mask_agg.columns = ['Mask', 'Password', 'Complexity', 'Length']
+        df_mask_agg.reset_index(drop=True)
+        df_mask_agg = df_mask_agg[df_mask_agg['Password'] > 1]
+        df_mask_agg.rename(columns={'Password': 'Count'}, inplace=True)
+
+        df_password_agg = df.groupby(by='Password').agg(
+            {'Password': 'count', 'Complexity': 'mean', 'Length': 'mean'}).rename_axis(None).sort_values(by='Password',
+                                                                                                         ascending=False).reset_index()
+        df_password_agg.columns = ['Password', 'Count', 'Complexity', 'Length']
+        df_password_agg.reset_index(drop=True)
+
+        if filter_lowqual:
+            df_mask_agg = df_mask_agg[df_mask_agg['Count'] > round(df_mask_agg.size * 0.001, 0)]
+            df_tokens = df_tokens[df_tokens['Count'] > round(df_tokens.size * 0.001, 0)]
+
+        self.print_stats(df, 'full')
+        self.print_stats(df_password_agg, 'password_agg')
+        self.print_stats(df_tokens, 'tokens')
+        self.print_stats(df_mask_agg, 'mask_agg')
+
+        if args.output:
+            df.to_csv(os.path.join(args.output, 'passwords.csv'), index=False)
+            df_tokens.to_csv(os.path.join(args.output, 'common_tokens.csv'), index=False)
+            df_class_agg.to_csv(os.path.join(args.output, 'password_classes.csv'), index=False)
+            df_mask_agg.to_csv(os.path.join(args.output, 'password_masks.csv'), index=False)
+            df_password_agg.to_csv(os.path.join(args.output, 'passwords_agg.csv'), index=False)
+
+    def print_stats(self, df2print, type_str):
+        """
+        Prints DF stats to CLI
+        :param df2print: pd.DataFrame to print
+        :param type_str: string that contains the type of DF being passed
+        """
+        if type_str == 'full':
+            message('Password Stats:', title=True)
+            message('Reminder the sample is ONLY cracked passwords and data points should be reflected on as so',
+                    title=True)
+            message(
+                'Microsoft minimum password complexity requires 3 of the following criteria: 1 lowercase, 1 uppercase, 1 digit, and 1 special character.',
+                title=True)
+            message('There are ' + message(str(len(df2print)), word=True) + ' passwords in the sample and the average complexity is ' + message(
+                str(round(df2print['Complexity'].mean(), 1)) + '/4',
+                word=True) + ' and the average length is ' + message(
+                str(round(df2print['Length'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '0: subpar'].count()),
+                            word=True) + ' passwords were considered subpar and did not meet minimum password requirements and had an average length of ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '0: subpar'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '1: minimum'].count()),
+                            word=True) + ' passwords met the minimum complexity requirements and had an average length of ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '1: minimum'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '2: okay'].count()),
+                            word=True) + ' passwords met or exceeded minimum complexity requirements and had an average length of ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '2: okay'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '3: good'].count()),
+                            word=True) + ' passwords met or exceeded minimum complexity requirements and had a strong password length averaging ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '3: good'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '4: great'].count()),
+                            word=True) + ' passwords met or exceeded minimum complexity requirements and had a very strong password length averaging ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '4: great'].mean(), 1)), word=True), stat=True)
+            message(message(str(df2print['Class'][df2print['Class'] == '5: strong'].count()),
+                            word=True) + ' passwords well exceeded minimum complexity requirements and had a fortified password length averaging ' + message(
+                str(round(df2print['Length'][df2print['Class'] == '5: strong'].mean(), 1)), word=True), stat=True)
+
+            if args.compare:
+                message('Password Lookup:', title=True)
+                for i in os.listdir(args.compare):
+                    try:
+                        message(message(str(df2print['Is In ' + str(i)].count()),
+                                        word=True) + ' passwords were also in ' + message(str(i), word=True), stat=True)
+                    except KeyError:
+                        pass
+
+        elif type_str == 'password_agg':
+            message('Reused Passwords:', title=True)
+            for i in range(0, 8):
+                message(message(str(df2print['Password'].iloc[i]), word=True) + ' occurred ' + message(
+                    str(df2print['Count'].iloc[i]), word=True) + ' times', stat=True)
+
+        elif type_str == 'tokens':
+            message('Common Tokens and Words in Passwords:', title=True)
+            for i in range(0, 8):
+                message('The token ' + message(str(df2print['Tokens'].iloc[i]), word=True) + ' was used ' + message(
+                    str(df2print['Count'].iloc[i]), word=True) + ' times', stat=True)
+
+        elif type_str == 'mask_agg':
+            message('Common Password Masks:', title=True)
+            for i in range(0, 8):
+                message(message(str(df2print['Count'].iloc[i]), word=True) + ' passwords used the mask ' + message(
+                    str(df2print['Mask'].iloc[i]), word=True), stat=True)
+                message('For example: ' + message(
+                    str(self.df['Password'][self.df['Mask'] == str(df2print['Mask'].iloc[i])].iloc[0]),
+                    word=True) + ', ' + message(
+                    str(self.df['Password'][self.df['Mask'] == str(df2print['Mask'].iloc[i])].iloc[1]),
+                    word=True) + ', and ' + message(
+                    str(self.df['Password'][self.df['Mask'] == str(df2print['Mask'].iloc[i])].iloc[2]), word=True),
+                        stat=True)
+
+
+if __name__ == '__main__':
+    # colorama
+    init()
+    parser = argparse.ArgumentParser(
+        description='Tool for identifying systemic password usage, creating password masks, and analyzing cracked password samples with human readable statistics')
+    parser.add_argument("-i", "--input", action="store", default=False, help='Input list of passwords.')
+    parser.add_argument("-c", "--compare", action="store", default=False,
+                        help='Directory of lists to compare against.')
+    parser.add_argument("-o", "--output", action="store", default=False,
+                        help="Prints CSV files to directory. The default is cwd.")
+    parser.add_argument("-f", "--filter", action="store_true", default=False,
+                        help="Filter subpar from results and bottom 0.1 percent of masks and tokens.")
+    parser.add_argument("-q", "--quiet", action="store_true", default=False,
+                        help="Hides banner")
+
+    dep_check()
+
+    args = parser.parse_args()
+    stemmer = PorterStemmer()
+
+    if args.output:
+        if args.output == '-':
+            args.output = os.getcwd()
+    if args.compare:
+        if args.compare == '-':
+            args.output = os.getcwd()
+
+    if not sys.stdin.isatty() and not args.input:
+        args.input = sys.stdin
+
+    try:
+        if not args.quiet:
+            message('', banner=True)
+        df = pd.read_table(args.input, header=None, names=['Password'])
+    except (FileNotFoundError, ValueError):
+        print('No input file found')
+        exit()
+
+    pwdAnalyzer = PasswordAnalyzer(df)
+    pwdAnalyzer.analyze_passwords()
+    pwdAnalyzer.lookup_directory(args.compare)
+    pwdAnalyzer.report(args.filter)
